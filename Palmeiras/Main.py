2e6d763f65e6d98a97f0ecb3c3e1e0c991122197
diego.mpavezzi <diego_marcondes@live.com>
2018-09-20 20:10:42 -0300

    Versão inicial do meu projeto

--- Palmeiras/Main.py
+++ Palmeiras/Main.py
@@ -1,3 +1,18 @@
-print("oi mundo")
-print("1 x 0 Palmeiras")
-print("creu")
+import math
+
+a = int(input("informe valor de a: "))
+b = int(input("informe valor de b: "))
+c = int(input("informe valor de c: "))
+
+delta = b * b - 4 * a * c
+
+print("Delta: ", delta)
+
+if delta >=0:
+    x1 = (- b + math.sqrt(delta)) / (2 * a)
+    x2 = (- b - math.sqrt(delta)) / (2 * a)
+    print("raiz x1: ", x1)
+    print("raiz x2: ", x2)
+
+else:
+    print("não existe raizes reias")
