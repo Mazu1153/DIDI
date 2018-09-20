e98faa3cf6d66498ca3dd18cc8308ce17fe6e569
diego.mpavezzi <diego_marcondes@live.com>
2018-09-20 20:26:00 -0300

    Versão inicial do meu projeto

---
+++ EX02/main.py
@@ -0,0 +1,7 @@
+import math
+x1=int(input("digite o primeiro numero: "))
+y1=int(input("digite o segundo numero: "))
+x2=int(input("digite o terceiro numero:1 "))
+y2=int(input("digite o quarto numero: "))
+d = math.sqrt((x1-x2)**2+(y1-y2)**2)
+print("a distancia dos pontos eh", d)
